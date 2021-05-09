04e822806bd3eaaf4761a657d76436652bc65fbd
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-09 21:49:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-if katz_deli > 0
+if katz_deli != 0
   katz_deli.map.with_index(1){|index, person| puts "The line is currently: #{index}. #{person}"}
 elsif katz_deli == 0
   puts "The line is currently empty."
