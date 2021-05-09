4a94195340b297486d3d28d75df3463a59b93b77
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-09 22:32:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+
+def now_serving(deli)
+  if deli.empty? 
+    puts "There is nobody waiting to be served!"
+  end
+  
+end
