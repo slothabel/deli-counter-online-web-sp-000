226ee61bfb8a85ac8ca5c37797b0061a0e0b3089
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-09 21:41:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  katz_deli.map.with_index(1){|index, person| puts "The line is currently: #{index} #{person}"}
+  puts "The line is currently empty."
+end
