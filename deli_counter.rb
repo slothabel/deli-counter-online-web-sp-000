2f8bf8c44310fcee1115280bfc6e2ecdcd3cc8c5
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-09 21:50:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,9 @@ katz_deli = []
 def line(katz_deli)
 if katz_deli != 0
   katz_deli.map.with_index(1){|index, person| puts "The line is currently: #{index}. #{person}"}
-elsif katz_deli == 0
-  puts "The line is currently empty."
-else
-  "no"
 end
+  puts "The line is currently empty."
+
 end
 
 def take_a_number(katz_deli, string)
